69fedf9d71becd47687664af69c0395ab3783cac
Eric <eric@erichedstrom.com>
2022-08-21 11:02:07 -0700

    Added logging to csv.

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+# ISP speed monitor
+
 # Import speedtest.net's speedtest-cli module.
 try:
     import speedtest
 except ImportError as error:
     print(error)
 
-# Import date time for human readable time.
+# Import date time for human-readable time.
 try:
     import datetime
+except ImportError as error:
+    print(error)
+
+# Import time for seconds calculation and sleep.
+try:
+    import time
 except ImportError as error:
     print(error)
 
@@ -16,66 +24,82 @@ try:
 except ImportError as error:
     print(error)
 
-# This script will run for X hour blocks.
-try:
-    import time
-except ImportError as error:
-    print(error)
-
-# This script will run for X hour blocks.
+# Use logging to display info messages to terminal.
 try:
     import logging
 except ImportError as error:
     print(error)
 
 # Local variables
+# This script will run for X hours.
 run_time_hours = 8
-log_path = "./log/"
+current_date = str(datetime.datetime.today().strftime('%Y-%m-%d'))
 
-# Logging basic config
-logging.basicConfig(
-    format='%(asctime)s %(levelname)-8s %(message)s',
-    level=logging.INFO,
-    datefmt='%Y-%m-%d %H:%M:%S')
+# Log information and configuration
+log_name = str("ispmon_" + current_date)
+log_path = "./log/" + log_name
+log_enabled = True
 
-# Start of epoc time.
-logging.info("ISP Monitor START")
-start_epoc = time.time()
 
-# Convert run time into minutes.
-run_time_seconds = run_time_hours * 60 * 60
+def main():
+    # Open an existing log or create a new one.
+    if log_enabled:
+        with open(log_path, "a+") as speed_log:
+            text = str(log_name + " LOG START UP, DOWN \n")
+            speed_log.write(text)
 
-# Run monitor until time has expired.
-end_epoc = start_epoc + run_time_seconds
+    # Logging basic config.
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        level=logging.INFO,
+        datefmt='%Y-%m-%d %H:%M:%S')
 
-# Convert to local time.
-datetime_time = datetime.datetime.fromtimestamp(end_epoc)
+    # Start of epoc time.
+    logging.info("ISP Monitor START")
+    start_epoc = time.time()
 
-# Display stop time to terminal.
-logging.info(f"ISP Monitor run until {datetime_time}")
+    # Convert run time into minutes.
+    run_time_seconds = run_time_hours * 3600
 
-# Main loop.
-while time.time() <= end_epoc:
+    # Run monitor until time has expired.
+    end_epoc = start_epoc + run_time_seconds
 
-    # Remote speed test.
-    try:
+    # Convert to local time.
+    datetime_time = datetime.datetime.fromtimestamp(end_epoc)
+
+    # Display stop time to terminal.
+    logging.info(f"ISP Monitor run until {datetime_time}")
+
+    while time.time() <= end_epoc:
+        # Remote speed test.
         speed = speedtest.Speedtest()
-    except:
-        print("Unable to perform speedtest()")
 
-    # Upload / download in megabytes.
-    upload = speed.upload() / 1024 / 1024
-    download = speed.download() / 1024 / 1024
+        # Upload / download in megabytes.
+        upload = speed.upload() / 1024 / 1024
+        download = speed.download() / 1024 / 1024
 
-    # Clean the numbers up for a human to view.
-    download = round(download, 1)
-    upload = round(upload, 1)
+        # Clean the numbers up for a human to view.
+        download = str(round(download, 1))
+        upload = str(round(upload, 1))
 
-    # Notify to terminal.
-    logging.info(f"UP: {upload} MB DOWN: {download} MB")
+        # Notify to terminal.
+        logging.info(f"UP: {upload} MB DOWN: {download} MB")
 
-    # We only want to check occasionally.
-    time.sleep(30)
+        # Write to log.
+        if log_enabled:
+            with open(log_path, "a+") as speed_log:
+                current_time = str(datetime.datetime.now())
+                text = current_time + ", " + upload + ", " + download + "\n"
+                speed_log.write(text)
+
+        # We only want to check occasionally.
+        time.sleep(30)
+
+
+# Run if not imported.
+if __name__ == "__main__":
+    main()
+
 
 
 
